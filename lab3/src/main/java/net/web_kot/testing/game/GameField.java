78b4e25f92a317496d8017b82807b5ed4727840e
Timofei Ananiev <kot@k-o-t.net>
2018-03-15 23:42:24 +0700

    TDD / Getter + setter for cell value

--- lab3/src/main/java/net/web_kot/testing/game/GameField.java
+++ lab3/src/main/java/net/web_kot/testing/game/GameField.java
@@ -9,5 +9,23 @@ public class GameField {
      * Game field width and height
      */
     public static final int SIZE = 4;
+
+    /**
+     * Set game field cell with 2^pow value
+     * @param cell field cell
+     * @param pow power value
+     */
+    public void setCellValue(Cell cell, int pow) {
+        
+    }
+
+    /**
+     * Returns log2 of value stored in cell
+     * @param cell field cell
+     * @return log2(value)
+     */
+    public int getCellValue(Cell cell) {
+        return 1;
+    }
     
 }
--- lab3/src/test/java/net/web_kot/testing/game/GameFieldTests.java
+++ lab3/src/test/java/net/web_kot/testing/game/GameFieldTests.java
@@ -1,5 +1,6 @@
 package net.web_kot.testing.game;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,16 @@ public class GameFieldTests {
     @DisplayName("Class initialization")
     public void testClassInitialization() {
         new GameField();
+    }
+    
+    @Test
+    @DisplayName("Set cell value")
+    public void testSetCellValue() {
+        Cell cell = new Cell(0, 0);
+        GameField field = new GameField();
+        
+        field.setCellValue(cell, 1);
+        Assertions.assertEquals(field.getCellValue(cell), 1);
     }
     
 }
