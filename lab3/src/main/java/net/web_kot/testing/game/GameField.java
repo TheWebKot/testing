bed89819a0b3275a4efefc6b2e5bcf393b9eaf69
Timofei Ananiev <kot@k-o-t.net>
2018-03-15 23:14:47 +0700

    TDD / Cell bounds checking tests (not passed)

--- lab3/src/main/java/net/web_kot/testing/game/GameField.java
+++ lab3/src/main/java/net/web_kot/testing/game/GameField.java
@@ -4,5 +4,10 @@ package net.web_kot.testing.game;
  * Representing a 512 game field
  */
 public class GameField {
+
+    /**
+     * Game field width and height
+     */
+    public static final int SIZE = 4;
     
 }
--- lab3/src/test/java/net/web_kot/testing/game/CellTests.java
+++ lab3/src/test/java/net/web_kot/testing/game/CellTests.java
@@ -1,6 +1,8 @@
 package net.web_kot.testing.game;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Cell")
@@ -10,6 +12,36 @@ public class CellTests {
     @DisplayName("Class initialization")
     public void testClassInitialization() {
         new Cell(0, 0);
+    }
+
+    @Nested
+    @DisplayName("Bounds checking")
+    public class TestInitializationBounds {
+        
+        @Test
+        @DisplayName("Row excess")
+        public void testRowExcess() {
+            Assertions.assertThrows(IndexOutOfBoundsException.class, () -> new Cell(GameField.SIZE + 1, 0));
+        }
+
+        @Test
+        @DisplayName("Column excess")
+        public void testColumnExcess() {
+            Assertions.assertThrows(IndexOutOfBoundsException.class, () -> new Cell(2, GameField.SIZE));
+        }
+
+        @Test
+        @DisplayName("Row negative value")
+        public void testRowNegative() {
+            Assertions.assertThrows(IndexOutOfBoundsException.class, () -> new Cell(-1, 1));
+        }
+
+        @Test
+        @DisplayName("Column negative value")
+        public void testColumnNegative() {
+            Assertions.assertThrows(IndexOutOfBoundsException.class, () -> new Cell(3, -3));
+        }
+        
     }
     
 }
