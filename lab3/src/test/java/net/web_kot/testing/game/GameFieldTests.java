2c209c61ca7e111b3ef6e4591a7db637828fdcf4
Timofei Ananiev <kot@k-o-t.net>
2018-03-15 23:44:48 +0700

    TDD / Impoved cell value tests (not passed)

--- lab3/src/test/java/net/web_kot/testing/game/GameFieldTests.java
+++ lab3/src/test/java/net/web_kot/testing/game/GameFieldTests.java
@@ -3,6 +3,11 @@ package net.web_kot.testing.game;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 @DisplayName("Game field")
 public class GameFieldTests {
@@ -13,14 +18,23 @@ public class GameFieldTests {
         new GameField();
     }
     
-    @Test
+    @ParameterizedTest
+    @MethodSource("cellValuesProvider")
     @DisplayName("Set cell value")
-    public void testSetCellValue() {
-        Cell cell = new Cell(0, 0);
+    public void testSetCellValue(int r, int c, int v) {
+        Cell cell = new Cell(r, c);
         GameField field = new GameField();
         
-        field.setCellValue(cell, 1);
-        Assertions.assertEquals(field.getCellValue(cell), 1);
+        field.setCellValue(cell, v);
+        Assertions.assertEquals(field.getCellValue(cell), v);
+    }
+    
+    private static Stream<Arguments> cellValuesProvider() {
+        return Stream.of(
+                Arguments.of(0, 0, 1),
+                Arguments.of(1, 2, 3),
+                Arguments.of(3, 3, 8)
+        );
     }
     
 }
